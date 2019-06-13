94c0808b7d05a035fc1a117cf6b2b8877b066c91
Jason Crawford <daxapps777@gmail.com>
2019-06-12 20:42:37 -0400

    display default mapview

---
+++ src/components/containers/Map.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+// import { View, Text } from 'react-native';
+import MapView from 'react-native-maps';
+// import { MapView } from 'expo';
+
+class Map extends Component {
+	render() {
+		return (
+			<MapView
+				style={{ flex: 1 }}
+				initialRegion={{
+					latitude: 37.78825,
+					longitude: -122.4324,
+					latitudeDelta: 0.0922,
+					longitudeDelta: 0.0421
+				}}
+			/>
+		);
+	}
+}
+
+export default Map;
